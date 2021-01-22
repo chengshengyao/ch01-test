2c1882704b4f177fdfa302e101407a19162be93d
chengshengyao <3113547732@QQ.COM>
2021-01-22 16:49:42 +0800

    这是我第一次用GIT

---
+++ src/com/bjpowernode/domain/Person.java
@@ -0,0 +1,15 @@
+package com.bjpowernode.domain;
+
+/**
+ * @ProjectName: test-hello-github
+ * @Package: com.bjpowernode.domain
+ * @Description: java类作用描述
+ * @Author: 生尧
+ * @CreateDate: 2021/1/22 16:25
+ * @Version: 1.0
+ * <p>
+ * Copyright: Copyright (c) 2021
+ */
+public class Person {
+    private String id ;
+}
